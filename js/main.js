aae45cb57945f5ce7006729b71a618c6e4cb1d4f
Anes Cehic <anes.cehic@gmail.com>
2018-12-12 01:59:31 +0100

    Implementing design

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".forma").on('submit', function(e) {
+    e.preventDefault();
+    let username = $("#username").val();
+    let password = $("#password").val();
+})
+
+$(document).ready(function() {
+    //ovdje ide kad se dom loada
+})
