e221cdd8b445478c9564f7d07e8cae3a0321cd5e
Anes Cehic <anes.cehic@gmail.com>
2018-12-12 22:36:16 +0100

    Implementing design

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 $(".forma").on('submit', function(e) {
-    e.preventDefault();
-    let username = $("#username").val();
-    let password = $("#password").val();
-    let logovan = false;
-    users.forEach(function(user){
-      if(user.username === username)
-        if(user.password === password){
-          logovan = true;
-          localStorage.setItem
-          location.assign('/game.html');
-        }
-    });
-    if(!logovan) {
-
+  e.preventDefault();
+  let username = $("#username").val();
+  let password = $("#password").val();
+  let logovan = false;
+  users.forEach(function(user){
+    if(user.username === username)
+    if(user.password === password){
+      logovan = true;
+      localStorage.setItem
+      location.assign('/game.html');
     }
-    //Ubaci jquery sa label paragrafima kao bootstrap sto ima alert
+  });
+  if(!logovan) {
+
+  }
+  //Ubaci jquery sa label paragrafima kao bootstrap sto ima alert
 
 })
 
 $(document).ready(function() {
     //ovdje ide kad se dom loada
+})
+
+$("#data").on("click", function() {
+  $.ajax({
+    url: "./db/users.json",
+    dataType: "json",
+    success: function(data) {
+      setting(data);
+    }
+  });
+  function setting(data) {
+    data.username = "ndsna";
+    console.log(data);
+  }
 })
