a9fd0f09be286c805d73eace0a4ee8798e829e62
Mirnel Bucan <mirnel.bucan@gmail.com>
2018-12-13 22:15:51 +0100

    board init added

--- js/generateBoard.js
+++ js/generateBoard.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   let a = 0;
+  const board = _initBoard();
+  console.log(board);
   //generating board
   for(let i = 1; i <= 15; i++) {
     $(".board").append('<div class="row row' + i + '"></div>');
     for (let j = 1; j <= 15; j++) {
-      $('.row' + i).append('<div class=column "' + i + ' ' + j + '"></div>');
+      $('.row' + i).append(`<div class="column ${i} ${j}"></div>`);
     }
   }
   $(".column").on('click', function(e) {
+    const [,col,row] = $(this).attr('class').split(' ');
+    console.log(col,row);
     if(a % 2 === 0) {
       $(this).css("background-color", "black");
       a = 1;
@@ -24,3 +28,16 @@ $(document).ready(function() {
     $(this).unbind('click');
   });
 });
+
+function _initBoard(){
+  const board = [];
+  for(let i = 0;i<15;i++){
+    board.push([]);
+    for(let j = 0; j<15 ; j++)
+      board[i].push("");
+  }
+  return board;
+}
+
+
+
