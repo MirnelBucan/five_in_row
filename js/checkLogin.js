a41cfe78c1741d1a1379e9d98946d7794b272253
Mirnel Bucan <mirnel.bucan@gmail.com>
2018-12-14 21:50:29 +0100

    code refactored a little bit

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,23 +1,21 @@
-(function checkLogin(){
+(function checkLogin() {
   let checkUsers = localStorage.getItem('users');
   let currentlyLogedIn = sessionStorage.getItem('user');
   let logovan = false;
-  console.log(checkUsers);
-  console.log(currentlyLogedIn);
-  if(checkUsers === null || currentlyLogedIn === null){
+  if (checkUsers === null || currentlyLogedIn === null) {
     location.assign('login.html');
     return;
   }
   checkUsers = JSON.parse(checkUsers);
   currentlyLogedIn = JSON.parse(currentlyLogedIn);
-  checkUsers.forEach(function(user){
-    if(user.username === currentlyLogedIn.username)
-      if(user.password === currentlyLogedIn.password){
-        logovan=true;
+  checkUsers.forEach(function(user) {
+    if (user.username === currentlyLogedIn.username)
+      if (user.password === currentlyLogedIn.password) {
+        logovan = true;
         return;
       }
   });
-  if(!logovan){
+  if (!logovan) {
     location.assign('login.html');
   }
 })();
